62352c8ebb46820a95e3feb3bb429e6aec4e3a83
“Silvio <silvio.dossantos@mercadolivre.com>
2022-11-30 13:32:57 -0300

    Criação do método @PostMapping para criação da categoria na class CategoriaController

--- src/main/java/com/ecommerce/natureba/controller/CategoriaController.java
+++ src/main/java/com/ecommerce/natureba/controller/CategoriaController.java
@@ -3,9 +3,11 @@ package com.ecommerce.natureba.controller;
 import com.ecommerce.natureba.model.Categoria;
 import com.ecommerce.natureba.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -21,5 +23,11 @@ public class CategoriaController {
     public ResponseEntity<List<Categoria>> getByCategoria(@PathVariable String categoria){
         return ResponseEntity.ok(categoriaRepository.findAllByCategoriaContainingIgnoreCase(categoria));
     }
+    @PostMapping
+    public ResponseEntity<Categoria> criarCategoria(@Valid @RequestBody Categoria categoria){
 
+             return ResponseEntity.status(HttpStatus.OK)
+                .body(categoriaRepository.save(categoria));
+
+    }
 }
