cf24c3f1158667256d395ee42c6045f18c4f314e
Brenda Bispo <115106207+brendabispo@users.noreply.github.com>
2022-11-30 12:12:30 -0300

    adicionando método buscar por id

--- src/main/java/com/ecommerce/natureba/controller/CategoriaController.java
+++ src/main/java/com/ecommerce/natureba/controller/CategoriaController.java
@@ -1,8 +1,13 @@
 package com.ecommerce.natureba.controller;
 
+import com.ecommerce.natureba.model.Categoria;
 import com.ecommerce.natureba.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +19,12 @@ public class CategoriaController {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
-
-
+    
+    @GetMapping("/{id}")
+	public ResponseEntity<Categoria> getById(@PathVariable Long id){
+		return categoriaRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
 
 }
