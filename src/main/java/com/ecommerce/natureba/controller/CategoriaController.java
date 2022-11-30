dd41b108386093fc058604b38f4c94cbe9ed0d4e
AdrianaMeli <adriana.nogueira@mercadolivre.com>
2022-11-30 14:58:57 -0300

    Endpoitn Listar

--- src/main/java/com/ecommerce/natureba/controller/CategoriaController.java
+++ src/main/java/com/ecommerce/natureba/controller/CategoriaController.java
@@ -23,7 +23,6 @@ public class CategoriaController {
 
     @GetMapping
     private ResponseEntity<List<Categoria>> getAll(){
-
         return ResponseEntity.ok(categoriaRepository.findAll());
     }
 
