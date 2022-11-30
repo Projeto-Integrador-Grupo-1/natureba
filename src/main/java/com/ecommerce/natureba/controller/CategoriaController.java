407a564a95dcec259275d23ea73c3179baab3b01
Vania Gomes <vania.vgomes@mercadolivre.com>
2022-11-30 14:49:02 -0300

    Subindo novamente o método atualizar

--- src/main/java/com/ecommerce/natureba/controller/CategoriaController.java
+++ src/main/java/com/ecommerce/natureba/controller/CategoriaController.java
@@ -3,9 +3,10 @@ package com.ecommerce.natureba.controller;
 import com.ecommerce.natureba.model.Categoria;
 import com.ecommerce.natureba.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -17,9 +18,33 @@ public class CategoriaController {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+
+    @GetMapping("/{id}")
+	  public ResponseEntity<Categoria> getById(@PathVariable Long id){
+		  return categoriaRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+
     @GetMapping("/categoria/{categoria}")
     public ResponseEntity<List<Categoria>> getByCategoria(@PathVariable String categoria){
         return ResponseEntity.ok(categoriaRepository.findAllByCategoriaContainingIgnoreCase(categoria));
     }
+    @PostMapping
+    public ResponseEntity<Categoria> criarCategoria(@Valid @RequestBody Categoria categoria) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(categoriaRepository.save(categoria));
+    }
 
+    @PutMapping
+    public ResponseEntity<Categoria> putCategoria(@Valid @RequestBody Categoria categoria) {
+
+        return categoriaRepository.findById(categoria.getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.Ok)
+                        .body(categoriaRepository.save(categoria))
+                .orElse(ResponseEntity.notFound().build());
+
+    }
 }
+
