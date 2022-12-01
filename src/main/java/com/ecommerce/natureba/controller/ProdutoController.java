0badff9e98d7a8e7b36c041e3211a852eab26930
Willian de Jesus Caetano <willian.caetano@mercadolivre.com>
2022-12-01 14:45:35 -0300

    atualizado classes controllers

--- src/main/java/com/ecommerce/natureba/controller/ProdutoController.java
+++ src/main/java/com/ecommerce/natureba/controller/ProdutoController.java
@@ -1,4 +1,21 @@
 package com.ecommerce.natureba.controller;
 
+import com.ecommerce.natureba.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produto")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProdutoController {
+
+    @Autowired
+    CategoriaRepository categoriaRepository;
+
+    @Autowired
+    ProdutoController produtoController;
+
+    
 }
