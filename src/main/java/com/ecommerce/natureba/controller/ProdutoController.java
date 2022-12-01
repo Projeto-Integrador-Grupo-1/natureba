ef8353eadd452955b0565c0a1904086f084677a4
AdrianaMeli <adriana.nogueira@mercadolivre.com>
2022-12-01 15:23:16 -0300

    inserindo endpoints do produtocontroller (Listar produtos, Listar produtos (id))

--- src/main/java/com/ecommerce/natureba/controller/ProdutoController.java
+++ src/main/java/com/ecommerce/natureba/controller/ProdutoController.java
@@ -1,10 +1,13 @@
 package com.ecommerce.natureba.controller;
 
+import com.ecommerce.natureba.model.Produto;
 import com.ecommerce.natureba.repository.CategoriaRepository;
+import com.ecommerce.natureba.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produto")
@@ -15,7 +18,18 @@ public class ProdutoController {
     CategoriaRepository categoriaRepository;
 
     @Autowired
-    ProdutoController produtoController;
+    ProdutoRepository produtoRepository;
 
+    @GetMapping
+    public ResponseEntity<List<Produto>> getAll(){
+        return ResponseEntity.ok(produtoRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> getById(@PathVariable Long id){
+        return produtoRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+    }
     
 }
