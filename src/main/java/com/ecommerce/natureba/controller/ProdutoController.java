9966fe031384aee71dc3b8eb0c249cc65b048f9b
Willian de Jesus Caetano <willian.caetano@mercadolivre.com>
2022-12-06 17:14:11 -0300

    Organizando estrutura do código

--- src/main/java/com/ecommerce/natureba/controller/ProdutoController.java
+++ src/main/java/com/ecommerce/natureba/controller/ProdutoController.java
@@ -1,4 +1,5 @@
 package com.ecommerce.natureba.controller;
+
 import com.ecommerce.natureba.model.Produto;
 import com.ecommerce.natureba.repository.CategoriaRepository;
 import com.ecommerce.natureba.repository.ProdutoRepository;
@@ -8,10 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
 import java.util.List;
 import javax.validation.Valid;
-
 
 @RestController
 @RequestMapping("/produto")
@@ -24,10 +23,41 @@ public class ProdutoController {
     @Autowired
     ProdutoRepository produtoRepository;
 
+    @GetMapping("/all")
+    public ResponseEntity<List<Produto>> getAll(){
+        return ResponseEntity.ok(produtoRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> getById(@PathVariable Long id){
+        return produtoRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
     @GetMapping("/nome/{nome}")
     public ResponseEntity<List<Produto>> getByName(@PathVariable String nome){
         return ResponseEntity.ok(produtoRepository.findAllByNomeContainingIgnoreCase(nome));
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> postProduto(@Valid @RequestBody Produto produto){
+        return categoriaRepository.findById(produto.getCategoria().getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto)))
+                .orElse(ResponseEntity.badRequest().build());
+    }
+
+    @PutMapping
+    public ResponseEntity<Produto> putProduto(@Valid @RequestBody Produto produto) {
+
+        if (produtoRepository.existsById(produto.getId())){
+
+            return categoriaRepository.findById(produto.getCategoria().getId())
+                    .map(resposta -> ResponseEntity.status(HttpStatus.OK).body(produtoRepository.save(produto)))
+                    .orElse(ResponseEntity.badRequest().build());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 
     @GetMapping("/preco_maior")
@@ -40,21 +70,6 @@ public class ProdutoController {
         return ResponseEntity.ok(produtoRepository.findAll(Sort.by(Sort.Direction.ASC,"preco")));
     }
 
-
-
-    @GetMapping("/all")
-    public ResponseEntity<List<Produto>> getAll(){
-        return ResponseEntity.ok(produtoRepository.findAll());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Produto> getById(@PathVariable Long id){
-        return produtoRepository.findById(id)
-                .map(resposta -> ResponseEntity.ok(resposta))
-                .orElse(ResponseEntity.notFound().build());
-    }
-    
-
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteProduto(@PathVariable Long id) {
 
@@ -66,24 +81,4 @@ public class ProdutoController {
                 })
                 .orElse(ResponseEntity.notFound().build());
     }
-
-    @PostMapping
-    public ResponseEntity<Produto> postProduto(@Valid @RequestBody Produto produto){
-        return categoriaRepository.findById(produto.getCategoria().getId())
-                .map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(produtoRepository.save(produto)))
-                .orElse(ResponseEntity.badRequest().build());
-    }
-    
-    @PutMapping
-	public ResponseEntity<Produto> putProduto(@Valid @RequestBody Produto produto) {
-					
-		if (produtoRepository.existsById(produto.getId())){
-
-			return categoriaRepository.findById(produto.getCategoria().getId())
-					.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(produtoRepository.save(produto)))
-					.orElse(ResponseEntity.badRequest().build());
-		}		
-		
-		return ResponseEntity.notFound().build();
-	}
 }
