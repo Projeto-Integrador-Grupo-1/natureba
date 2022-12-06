b5089043ae78b6ffc3685d21e22bb2444a869cde
AdrianaMeli <adriana.nogueira@mercadolivre.com>
2022-12-06 11:00:35 -0300

    Inserindo Usuario service Cadastrar Usuario

---
+++ src/main/java/com/ecommerce/natureba/service/UsuarioService.java
@@ -0,0 +1,42 @@
+package com.ecommerce.natureba.service;
+
+import com.ecommerce.natureba.model.Usuario;
+import com.ecommerce.natureba.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
+
+        if (usuarioRepository.findByUsuario(usuario.getUsuario()).isPresent())
+            return Optional.empty();
+
+
+
+        if (usuario.getFoto().isBlank())
+            usuario.setFoto("https://i.imgur.com/Zz4rzVR.png");
+
+
+        usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+        return Optional.of(usuarioRepository.save(usuario));
+
+    }
+    private String criptografarSenha(String senha) {
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        return encoder.encode(senha);
+    }
+
+
+}
