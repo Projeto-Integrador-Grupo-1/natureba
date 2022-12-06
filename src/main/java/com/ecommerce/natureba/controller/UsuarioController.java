dac6d47067f8d5cc0f237531766408a3b8ed901c
Willian de Jesus Caetano <willian.caetano@mercadolivre.com>
2022-12-06 11:08:57 -0300

    criado métodos listar todos e listar por ID.Willian_Caetano

--- src/main/java/com/ecommerce/natureba/controller/UsuarioController.java
+++ src/main/java/com/ecommerce/natureba/controller/UsuarioController.java
@@ -1,12 +1,34 @@
 package com.ecommerce.natureba.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.natureba.model.Usuario;
+import com.ecommerce.natureba.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Usuario>> getAll(){
+        return ResponseEntity.ok(usuarioRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id){
+        return usuarioRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
 }
